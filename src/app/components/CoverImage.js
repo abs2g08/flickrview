41da9b8be377fd32069ba416b7d870863f80a506
Adam <abs2g08@gmail.com>
2016-03-07 15:44:04 +0000

    adding cover image

---
+++ src/app/components/CoverImage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class CoverImage extends React.Component {
+  render() {
+    const src = this.props.src;
+
+    const imageStyle = {
+      background: `url(\'${src}\') no-repeat center center`,
+      backgroundSize: 'cover',
+      height: '200px'
+    };
+
+    return (
+      <div className='img-cover' style={imageStyle}></div>
+    );
+  }
+}
+
+CoverImage.propTypes = {
+  src: React.PropTypes.string
+};
